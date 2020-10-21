154969a1dd93b7a29973ef72479478fb09d5c87f
TrPakov <trpakov@abv.bg>
2020-10-21 21:27:42 +0300

    Direct Substitution error handling

--- Cryptography Exercises/DirectSubstitution.cs
+++ Cryptography Exercises/DirectSubstitution.cs
@@ -7,18 +7,25 @@ namespace Cryptography_Exercises
     static class DirectSubstitution
     {
         public static char[] M { get; set; } = "АБВГДЕИКЛМНОПРСТУФХЯ0123456789NYUD №-Ъ\"S".ToCharArray();
-        public static Dictionary<char, int> SubstitutionCodes { get; private set; } = new Dictionary<char, int>(M.Length);
+        public static Dictionary<char, int> SubstitutionCodes { get; private set; }
         public const string TestText = "ОТ СМЕТКА С № 320 201 642 881 7 НА БАНКА \"БИОХИМ\" ДА СЕ ПРЕХВЪРЛЯТ USD 10 000 000 В КОРЕСПОНДЕНТСКА БАНКА NY 20361-8975";
         public const string TestCrypto = "12 16 35 15 10 6 16 8 1 35 15 35 36 35 24 23 21 35 23 21 22 35 27 25 23 35 29 29 22 35 28 35 11 1 35 2 1 11 8 1 35 39 2 7 12 19 7 10 39 35 5 1 35 15 6 35 13 14 6 19 3 38 14 9 20 16 35 33 40 34 35 22 21 35 21 21 21 35 21 21 21 35 3 35 8 12 14 6 15 13 12 11 5 6 11 16 15 8 1 35 2 1 11 8 1 35 31 32 35 23 21 24 27 22 37 29 30 28 26";
 
         static DirectSubstitution()
         {
+            InitializeDictionary();
+        }
+
+        private static void InitializeDictionary()
+        {
+            SubstitutionCodes = new Dictionary<char, int>(M.Length);
             for (int i = 0; i < M.Length; i++)
                 SubstitutionCodes[M[i]] = i + 1;
         }
 
         public static Tuple<string, string> Encrypt(string plainText = TestText)
         {
+            InitializeDictionary();
             if (plainText.Length > 300) throw new ArgumentOutOfRangeException(plainText, "Допуска се явен текст с дължина не по-голяма от 300 символа.");
 
             var plainTextAsChars = plainText.ToCharArray();
@@ -43,6 +50,8 @@ namespace Cryptography_Exercises
 
         public static string Decrypt(string cryptogram, bool areSpacesEncrypted = true)
         {
+            InitializeDictionary();
+
             string[] input;
 
             if (areSpacesEncrypted)
@@ -52,7 +61,14 @@ namespace Cryptography_Exercises
 
                 for (int i = 0; i < input.Length; i++)
                 {
-                    result[i] = SubstitutionCodes.Single(x => x.Value == int.Parse(input[i])).Key;
+                    var isNum = int.TryParse(input[i], out int num);
+                    if (!isNum)
+                        throw new ArgumentException("Криптограмата съдържа символи, които не са числа.");
+
+                    result[i] = SubstitutionCodes.SingleOrDefault(x => x.Value == num).Key;
+
+                    if (result[i] == 0)
+                        throw new ArgumentException("Тази криптограма е създадена с по-голямо множество допустими стойности от настоящето.");
                 }
 
                 return new String(result);
